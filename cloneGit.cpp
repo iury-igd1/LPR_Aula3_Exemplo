811793b65e96bc4f9cfabee2e4b9e17c74b0432c
iury-igd1 <iurygoncalvessrs1@gmail.com>
2026-04-08 10:10:07 -0300

    commitClone

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
+
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m PARABÉNS, Iury!                    \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mEu clonei um repositório público do Git       \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mpara o meu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;35mEditado por mim!                              \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
 
